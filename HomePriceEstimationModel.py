42ab84ba5859c4d18ffaef3dcc39f7055eccd9cd
Colin <164561623+cesmit27@users.noreply.github.com>
2024-09-03 22:20:31 -0400

    Update HomePriceEstimationModel.py
    Forgot to add matplotlib.pyplot to the script

--- HomePriceEstimationModel.py
+++ HomePriceEstimationModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
